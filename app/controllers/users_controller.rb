af7678ff1e0478234b3e93db29e6f1de4d52ecc8
EC2 Default User <ec2-user@ip-172-31-21-252.ec2.internal>
2022-03-12 08:11:56 +0000

    [finish]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,12 @@ class UsersController < ApplicationController
   
   def edit
    @user = User.find(params[:id])
-   @user = current_user
+  
+   if @user == current_user
+      render :edit
+   else
+      redirect_to user_path(current_user)
+   end
   end
   
   def update
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_one_attached :image
+  has_one_attached :profile_image
  
-  
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 20}
                  
   
   validates :introduction, length: { maximum: 50 }
@@ -14,6 +14,13 @@ class User < ApplicationRecord
          
   has_many :books, dependent: :destroy
   
+  def get_profile_image
+   unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+   end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
 
   
 
